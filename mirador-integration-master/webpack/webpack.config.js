2ed657fabdb219c3e6648ba036bc7f10385e4e44
Max Resch <max.resch@donau-uni.ac.at>
2024-05-26 16:21:05 +0200

    [deployment] update environment parameter for webpack

--- mirador-integration-master/webpack/webpack.config.js
+++ mirador-integration-master/webpack/webpack.config.js
@@ -1,13 +1,13 @@
 const path = require('path');
 
-module.exports = deployment => {
-  let basename = path.join('dist', deployment ? path.basename(deployment) : '');
+module.exports = (env, args) => {
+  let basename = path.join('dist', env.deployment ? path.basename(env.deployment) : '');
   return {
     entry: './src/index.js',
     output: {
       filename: 'main.js',
       path: path.resolve(__dirname, basename),
-      publicPath: deployment || './dist/',
+      publicPath: env.deployment || './dist/',
     },
   };
 };
